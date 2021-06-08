b7a4e735a7524abad0357753d842fe9f19545e74
MikeRuk05 <misharuk05@mail.ru>
2021-06-08 21:15:32 +0300

    Added start menu

---
+++ Assets/Scripts/PipesController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipesController : MonoBehaviour
+{
+    [SerializeField] private GameObject pipes1, pipes2, pipeUp1, pipeUp2, pipeDown1, pipeDown2;
+    [SerializeField] private float maxUpPos, minUpPos, maxDownPos, minDownPos, minSpace, maxSpace, speed;
+    void Start()
+    {
+        while (true)
+        {
+            pipeUp1.transform.position = new Vector3(pipeUp1.transform.position.x, Random.Range(minUpPos, maxUpPos), pipeUp1.transform.position.z);
+            pipeDown1.transform.position = new Vector3(pipeDown1.transform.position.x, Random.Range(minDownPos, maxDownPos), pipeDown1.transform.position.z);
+            if (pipeUp1.transform.position.y - pipeDown1.transform.position.y > minSpace && pipeUp1.transform.position.y - pipeDown1.transform.position.y < maxSpace) break;
+        }
+        while (true)
+        {
+            pipeUp2.transform.position = new Vector3(pipeUp2.transform.position.x, Random.Range(minUpPos, maxUpPos), pipeUp2.transform.position.z);
+            pipeDown2.transform.position = new Vector3(pipeDown2.transform.position.x, Random.Range(minDownPos, maxDownPos), pipeDown2.transform.position.z);
+            if (pipeUp2.transform.position.y - pipeDown2.transform.position.y > minSpace && pipeUp2.transform.position.y - pipeDown2.transform.position.y < maxSpace) break;
+        }
+    }
+    void FixedUpdate()
+    {
+        pipes1.transform.position -= new Vector3(speed * Time.fixedDeltaTime, 0f, 0f);
+        pipes2.transform.position -= new Vector3(speed * Time.fixedDeltaTime, 0f, 0f);
+        if (pipes1.transform.position.x <= -4f) SpawnNew1();
+        if (pipes2.transform.position.x <= -4f) SpawnNew2();
+    }
+
+    void SpawnNew1()
+    {
+        pipes1.transform.position = new Vector3(4f, pipes1.transform.position.y, pipes1.transform.position.z);
+        while (true)
+        {
+            pipeUp1.transform.position = new Vector3(pipeUp1.transform.position.x, Random.Range(minUpPos, maxUpPos), pipeUp1.transform.position.z);
+            pipeDown1.transform.position = new Vector3(pipeDown1.transform.position.x, Random.Range(minDownPos, maxDownPos), pipeDown1.transform.position.z);
+            if(pipeUp1.transform.position.y - pipeDown1.transform.position.y > minSpace && pipeUp1.transform.position.y - pipeDown1.transform.position.y < maxSpace) break;
+        }
+    }
+
+    void SpawnNew2()
+    {
+        pipes2.transform.position = new Vector3(4f, pipes2.transform.position.y, pipes2.transform.position.z);
+        while (true)
+        {
+            pipeUp2.transform.position = new Vector3(pipeUp2.transform.position.x, Random.Range(minUpPos, maxUpPos), pipeUp2.transform.position.z);
+            pipeDown2.transform.position = new Vector3(pipeDown2.transform.position.x, Random.Range(minDownPos, maxDownPos), pipeDown2.transform.position.z);
+            if (pipeUp2.transform.position.y - pipeDown2.transform.position.y > minSpace && pipeUp2.transform.position.y - pipeDown2.transform.position.y < maxSpace) break;
+        }
+    }
+}
---
+++ Assets/Scripts/StartTextController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartTextController : MonoBehaviour
+{
+    [SerializeField] private Text startTxt;
+    [SerializeField] private Outline ol;
+    private float alpha;
+    void Start()
+    {
+    }
+    void FixedUpdate()
+    {
+        alpha = Mathf.PingPong(Time.time / 1.5f, 1f);
+        startTxt.color = new Color(startTxt.color.r, startTxt.color.g, startTxt.color.b, alpha);
+        ol.effectColor = new Color(ol.effectColor.r, ol.effectColor.g, ol.effectColor.b, alpha);
+    }
+}
